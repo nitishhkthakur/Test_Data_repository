9c8d303bc47919e7dd152235a988caa2c1ab6738
nitishhkthakur <Nitish.Thakur@otsi.ca.gov>
2025-02-06 12:21:55 +0530

    Add files via upload

---
+++ ScreenConstants (1).java
@@ -0,0 +1,218 @@
+
+package cares.cwds.salesforce.constants;
+
+public class ScreenConstants {	
+	
+	private ScreenConstants() {
+		
+	}
+	
+	/********************************Service*************************************/
+	public static final String SERVICESCATEGORY = "ServicesCategory";
+	public static final String SERVICESNAMEEBP = "ServiceNameEBP";
+	public static final String SERVICETYPE = "ServiceType";
+	
+	/********************************Common*************************************/
+	public static final String LOGIN = "Login";
+	public static final String HOME = "Home";
+	public static final String HEADERSPAGE = "Header";
+	public static final String LOGOUT = "Logout";
+	
+	public static final String DOCUMENTS = "Documents";
+	public static final String APPROVALAUDITHISTORY = "ApprovalAuditHistory";
+	public static final String HISTORY = "ScreeningHistory";
+	public static final String ORGANIZATIONS = "Organization";
+	public static final String ADDRESSES = "Addresses";
+	public static final String LOCATION = "Location";
+	public static final String RELATEDPERSONS = "RelatedPersons";
+	public static final String CORRESPONDENCES = "Correspondences";
+	public static final String RECALLANDRESUBMIT = "RecallAndResubmitRecord";
+	
+	/*******************************Screening************************************/
+	public static final String SCREENINGHEADER = "ScreeningsHeader";
+	public static final String SCREENINGSPAGE = "ScreeningsPage";
+	public static final String SCREENINGDETAILS = "ScreeningDetails";
+	public static final String SAFELYSURRENDEREDBABY = "ScreeningSafelySurrenderedBaby";
+	public static final String SCREENINGPERSONS = "ScreeningPersons";
+	public static final String SCREENINGVALIDATEPERSON = "ScreeningValidatePerson";
+	public static final String SCREENINGADDRESSES = "ScreeningAddresses";
+	public static final String CALLBACKATTEMPTS = "ScreeningCallBackAttempts";
+	public static final String SCREENINGALLEGATIONS = "ScreeningAllegations";
+	public static final String SCREENINGSAFETYALERTS = "ScreeningSafetyAlerts";
+	public static final String SCREENINGASSESSMENTS = "ScreeningAssessments";
+	public static final String SCRTRIBALINQUIRYANDCOLLABORATION = "ScreeningTribalInquiry";
+	public static final String SCREENINGCONTACTLOG = "ScreeningContactLog";
+	public static final String SCREENINGERR = "ScreeningERR";
+	public static final String SUPERVISORREVIEWPROMOTION = "ScreeningSupervisorReview";
+	public static final String SCREENINGSUBMITFORAPPROVAL = "ScreeningSubmitForApproval";
+	public static final String SCREENINGAPPROVAL = "ScreeningApproval";
+
+	/*******************************Folio************************************/	
+	public static final String FOLIOALLIGATIONS = "FolioAllegations";
+	public static final String FOLIODISPOSITION = "FolioDisposition";
+	public static final String FOLIOCOMMERCIALSEXUALEXPLOITATION = "FolioCommercialSexualExploite";
+	public static final String FATALITYNEARFATALITY = "FatalityNearFatality";
+	public static final String ADMINISTRATIVEHEARINGS = "AdministrativeHearings";
+	public static final String FOLIODETAILS = "FolioDetails";
+	public static final String FOLIOASSIGNMENTS = "Assignments";
+	public static final String FOLIOCORRESPONDENCE = "FolioCorrespondence";
+	public static final String FOLIOHISTORY = "FolioHistory";
+	public static final String FOLIOSCREENINGREVIEW = "FolioScreeningReview";
+	public static final String FOLIOTRIBALINQUIRYANDCOLLABORATION = "FolioTribalInquiry";
+	public static final String FOLIOCONTACTLOG = "FolioContactLog";
+	public static final String FOLIODOCUMENTS = "FolioDOcuments";
+	public static final String FOLIOAPPROVALANDAUDITHISTORY = "FolioApprovalAndAuditHistory";
+	public static final String RELATEDFOLIOS = "RelatedFolios";
+	public static final String SCREENINGSINFOLIO = "ScreeningsInFolio";
+	public static final String FOLIOATTORNEYCLIENTLOG = "FolioAttorneyClientLog";
+	public static final String FOLIOSUBMITFORAPPROVAL = "FolioSubmitForApproval";
+	public static final String FOLIOCHANGEASSIGNMENTS = "FolioChangeAssignments";
+	public static final String FOLIOPERSONS = "FolioPerson";
+	public static final String FOLIOVALIDATEPERSON ="FolioValidatePerson";
+	public static final String FOLIOADDENDUMS ="FolioAddendums";
+	public static final String FOLIONEWCASE ="FolioNewCase";
+	public static final String FOLIOPLACEMENT ="FolioPlacement";
+	public static final String FOLIOCHILDLOCATIONS ="FolioChildLocations";
+	public static final String FOLIOCHILDLOCATIONABSENCES ="FolioChildLocationAbsences";
+	public static final String CHILDLOCATIONCORRESPONDENCES ="ChildLocationCorrespondences";
+	public static final String PLACEMENTPRESERVATIONSTRATEGY ="PlacementPreservationStrategy";
+	public static final String FOLIOSERVICECOMPONENTS ="FolioServiceComponents";
+	public static final String FOLIORELEASESOFINFORMATIONANDCONSENTFORM ="FolioReleasesOfInformation";
+	public static final String FOLIOVISITLOGS ="FolioVisitLogs";
+	public static final String FOLIOCASECLOSURE ="FolioCaseClosure";
+	public static final String FOLIOCHILDLOCATIONCONTACTLOGS ="FolioChildLocationContactLogs";
+	public static final String FOLIOPLACEMENTNEEDS ="FolioPlacementNeeds";
+	public static final String FOLIOTRIBEPLACEMENTPREFERENCES ="FolioTribePlacementPreferences";
+	public static final String PLACEMENTSTABILITY ="PlacementStability";
+
+	public static final String LOCATIONSERVICESORG = "LocationServicesOrg";
+	public static final String CREDENTIALSORG = "CredentialsOrg";
+	public static final String SPECIALIZATIONORG = "SpecializationsOrg";
+	public static final String FOLIOERR = "FolioERR";
+	public static final String FOLIOFAMILYTRANSFERTYPE = "FolioFamilyTransfer";
+	public static final String FOLIOCASETYPE = "FolioCaseType";
+	public static final String FOLIOCASESTOTRANSFER ="FolioCasesToTransfer";
+	public static final String FOLIOFAMILYTRANSFERRECORD = "FolioFamilyTransferRecord";
+	
+	public static final String PROVIDERSEARCH = "ProviderSearch";
+	public static final String PROVIDEROPTIONS = "ProviderOptions";
+	public static final String FINDAWORKER = "FindAWorker";
+	public static final String NEWITEMSERVICEEEQUEST = "";
+	public static final String CASESTOTRANSFER = "CasestoTransfers";
+	
+	public static final String FOLIOTASK = "FolioTask";
+	public static final String VISITATIONDETERMINATIONS = "VisitationDeterminations";
+	public static final String ESIGN = "Esign";
+	public static final String PARENTGUARDAIANPLACEMENTPREFERENCE = "ParentGuardianPlacementPreferences";
+	public static final String YOUTHPLACEMENTPREFERENCE= "YouthPlacementPreferences";
+	public static final String TRIBALPLACEMENTPREFERENCE= "TribalPlacementPreferences";
+	
+	public static final String BACKGROUNDCHECKS = "BackgroundChecks";
+	public static final String FOLIOCHILDLOCATIONRATES ="FolioChildLocationRates";
+	
+	public static final String RELEASESOFINFORMATIONANDCONSENTFORM = "ReleasesOfInformationAndConsentForm";
+	public static final String REMOVAL ="Removal";
+	public static final String FOLIOCHILDLOCATIONAUDITHISTORY ="FolioChildLocationAuditHistory";
+	public static final String FOLIOBACKGROUNDCHECKS = "FolioBackgroundChecks";
+	public static final String  FOLIOCHILDLOCATIONOVERSTAYREPORTS="FolioChildLocationOverstayReports";
+	public static final String LEGALAUTHORITY ="Legal Authority";
+	
+	public static final String SPECIALIZATION ="Specialization";
+	
+	public static final String FOLIOINSPECTIONS = "FolioInspections";
+	public static final String FOLIOPROMOTETOCASE = "FolioPromoteToCase";
+	public static final String RECORDALERTSANDTASKS = "RecordAlertsAndTasks";
+	/*******************************Person************************************/
+	public static final String PERSONSEARCH = "PersonSearch";
+	public static final String PERSONCONTACTLOGMEMBERS = "PersonContactLogMembers";
+	public static final String PERSONAKA = "PersonAKA";
+	public static final String PERSONDETAILS = "PersonDetails";
+	public static final String PERSONPHONE="PersonPhone";
+	public static final String PERSONHISTORY = "PersonHistory";
+	public static final String ACTIVEPERSONADDRESS="ActivePersonAddress";
+	public static final String INACTIVEPERSONADDRESS="InactivePersonAddress";
+	public static final String PERSONTRIBALINFORMATION = "PersonTribalInformation";
+	public static final String PERSONHOUSEHOLDMEMBERSHIPS ="PersonHouseholdMemberships";
+	public static final String PERSONRELATIONSHIPS ="PersonRelationships";
+	public static final String PERSONADDRESSES = "PersonAddresses";
+	public static final String PERSONBIBACKGROUNDCHECKS = "PersonBIBackgroundChecks";
+	public static final String PERSONBIROI = "PersonBIROI";
+	public static final String PERSONBIJSI = "PersonBIJSI";
+	public static final String PERSONEIEDUCATION = "PersonEIEducation";
+	public static final String PERSONEIPOSTEDUCATIONSUPPORT = "PostSecondaryEducationSupport";
+	public static final String PERSONBHDIAGNOSIS = "PersonBHDiagnosis";
+	public static final String PERSONBEHAVIORALEMOTIONALHEALTHNEEDS  = "PersonBehavioralEmotionalNeeds";
+	public static final String PERSONALSTRENGTHSCOPINGSKILLS  = "PersonalStrengthsCopingSkills";
+	public static final String SUBSTANCEDEPENDENCYTREATMENT  = "SubstanceDependencyTreatment";
+	public static final String ALCOHOLDEPENDENCYTREATMENT  = "AlcoholDependencyTreatment";
+	public static final String PERSONLEGALISSUES  = "PersonLegalIssues";
+	public static final String PERSONHEALTHINFORMATION = "PersonHealthInformation";
+	public static final String PERSONMEDICALEQUIPMENTREQUIREDINFORMATION ="MedicalEquipmentRequiredInformation";
+	public static final String PERSONMEDICALEXAMSINFORMATION ="PersonMedicalExamsInformation";
+	public static final String PERSONMEDICATIONSINFORMATION ="PersonMedicationsInformation";
+
+	public static final String PERSONHEALTHALLERGIES ="PersonHealthAllergies";
+	public static final String PERSONDENTALEXAM ="PersonDentalExam";
+	public static final String PERSONHEARINGIMPAIRMENT ="PersonHearingImpairment";
+	public static final String PERSONHOSPITALIZATIONS ="PersonHospitalizations"; 
+	public static final String PERSONIMMUNIZATIONS ="PersonImmunizations";
+
+	public static final String PERSONPHYSICALHEALTHVISUALIMPAIREMENT  = "PersonPHVisualImpairement";
+	public static final String PERSONHEALTHINSURANCEMEDICAL  = "PersonHIMediCal";
+	public static final String PERSONHEALTHINSURANCEMEDICAID  = "PersonHIMedicaid";
+	public static final String OTHERHEALTHINSURANCECOVERAGE  = "OtherHealthInsuranceCoverage";
+	public static final String PERSONPRESCRIPTIONDRUGS  = "PersonPrescriptionDrugs";
+	
+	public static final String PERSONREGIONALCENTERINFORMATION = "PersonRegionalCenterInformation";
+	public static final String PERSONPREGNANACY = "PersonPregnancy";
+	public static final String PERSONSPECIALDIETREQUIRED = "PersonSpecialDietRequired";
+	public static final String PERSONMENTALHEALTH ="PersonMentalHealth";
+	public static final String PERSONSOCIALHISTORY = "PersonSocialHistory";
+	
+    public static final String PERSONALLERGIES = "PersonAllergies";
+	public static final String PERSONALCOHOLORSUBSTANCE = "AlcoholOrSubstanceUse";
+	public static final String PERSONHEARINGIMPAIRMENTANDDEAFNESS = "HearingImpairmentAndDeafness";
+	public static final String PRIORADOPTIONANDGUARDIANSHIP = "PriorAdoptionAndGuardianship";
+	public static final String PRIORADOPTION = "PriorAdoption";
+	public static final String PRIORGUARDIANSHIP = "PriorGuardianship";
+	public static final String SCHOOLDISCIPLINARYISSUES = "SchoolDisciplinaryIssues";
+
+    public static final String PERSONRELATED = "PersonRelated";
+	/*******************************CASE************************************/
+	public static final String NEWCASE = "NewCase";
+	public static final String CASEASSIGNMENTS = "CaseAssignments";
+	public static final String CASECHANGEASSIGNMENTS = "CaseChangeAssignments";
+	public static final String CASETASK = "CaseTask";
+	public static final String CASEDETAILS = "CaseDetails";
+	public static final String CASESUBMITFORAPPROVAL = "CaseSubmitForApproval";
+	public static final String CASEAPPROVALHISTORY = "CaseApprovalHistory";
+	public static final String CASERELATEDFOLIOS = "CaseRelatedFolios";
+	public static final String CASEFOLIOPERSONS = "CaseFolioPersons";
+	public static final String CASETRIBALINQUIRYANDCOLLABORATION = "CaseTribalInquiry";
+	public static final String CASECONTACTLOG = "CaseContactLog";
+	/*******************************Courts************************************/
+	public static final String PETITIONALLEGATION = "PetitionAllegation";
+	public static final String GENERATEDOCUMENT = "GenerateDocument";
+	public static final String GENERATEPETITION = "GeneratePetition";
+
+	public static final String AUDITHISTORY = "AuditHistory";
+	public static final String COURTSAPPROVALAUDITHISTORY = "CourtsApprovalAuditHistory";
+	public static final String COURTOFFICERNOTES = "CourtOfficerNotes";
+	public static final String COURTREPORTS = "CourtReports";
+	public static final String COURTMINUTEORDER = "CourtMinuteOrder";
+	public static final String DOCUMENTDISTRIBUTIONS = "DocumentDistributions";
+	public static final String RELATEDFOLIO = "RelatedFolios";
+	public static final String COURTWORKITEMS = "CourtWorkItems";
+	
+	/*********************** Organization ***********************************/
+	public static final String ORGANIZATIONSPROVIDERSEARCH = "OrganizationProviderSearch";
+
+
+	
+}	
+
+
+
+
+
+
